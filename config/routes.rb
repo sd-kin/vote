8e829cfc50c2d1ebd9454275077e75dea8a7449e
sd-kin <sd-kin@rambler.ru>
2016-04-09 21:04:25 +0000

    generate controllers for Poll and Options and declare routes

--- config/routes.rb
+++ config/routes.rb
@@ -54,4 +54,8 @@ Rails.application.routes.draw do
   #     # (app/controllers/admin/products_controller.rb)
   #     resources :products
   #   end
+
+  resources :polls do
+    resources :options
+  end
 end
