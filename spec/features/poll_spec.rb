cc5cc48a4faabe825afa0b5d42aba879612d0572
sd-kin <sd-kin@rambler.ru>
2016-04-24 20:56:31 +0000

    rename feature spec file for poll and add some correct feature specs

--- spec/features/poll_spec.rb
+++ spec/features/poll_spec.rb
@@ -1,27 +1,29 @@
 require 'rails_helper'
 
-feature 'creating poll' do
-  scenario 'visit home page' do
+feature 'when create poll' do
+  scenario 'should visit home page' do
     visit root_path
 
     expect(page).to have_content('Hello world')
   end
 
-  scenario 'visit create poll page' do
+  scenario 'should visit create poll page' do
     visit new_poll_path
 
     expect(page).to have_content('New poll page')
   end
 
-  scenario 'create poll', js: true do
+  scenario 'should create poll with title', js: true do
     visit new_poll_path
     fill_in 'poll_title', with: 'poll title'
     click_button 'Ok'
 
     expect(page).to have_content('poll title')
   end
+end
 
-  scenario 'create poll with two options', js: true  do
+feature 'when create options for poll', js: true do 
+  scenario 'should create poll with two options' do
     visit new_poll_path
     fill_in 'poll_title', with: 'poll title'
     click_button 'Ok'
@@ -39,28 +41,30 @@ feature 'creating poll' do
     expect(page).to have_content('option 2 description')
   end
 
-  scenario 'can not create poll without title' do
+  scenario 'should not create poll without title' do
     visit new_poll_path
-    click_button 'OK'
+    click_button 'Ok'
 
     expect(page).to have_content("Title can't be blank")
   end
 
-  scenario 'can not create poll without option title' do
+  scenario 'should not create poll without option title' do
     visit new_poll_path
     fill_in 'poll_title', with: 'poll title'
-    fill_in 'Description', with: 'option description'
-    click_button 'OK'
+    click_button 'Ok'
+    fill_in 'option_description', with: 'option 1 description'
+    click_button 'Add'
 
-    expect(page).to have_content("Options can't be blank")
+    expect(page).to have_content("Title can't be blank")
   end
 
   scenario 'can not create poll without description' do
     visit new_poll_path
     fill_in 'poll_title', with: 'poll title'
-    fill_in 'poll_options_attributes_0_title', with: 'option title'
-    click_button 'OK'
+    click_button 'Ok'
+    fill_in 'option_title', with: 'option 1 title'
+    click_button 'Add'
 
-    expect(page).to have_content("Options can't be blank")
+    expect(page).to have_content("Description can't be blank")
   end
 end
