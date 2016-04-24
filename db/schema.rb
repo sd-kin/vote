2935d1ee24e40c6e34d709881154477aa60ea30d
sd-kin <sd-kin@rambler.ru>
2016-04-24 20:55:14 +0000

    correct db schema

--- db/schema.rb
+++ db/schema.rb
@@ -11,21 +11,29 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20_151_227_213_610) do
+ActiveRecord::Schema.define(version: 20151227213610) do
+
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'options', force: :cascade do |t|
-    t.string   'title'
-    t.text     'description'
-    t.datetime 'created_at',  null: false
-    t.datetime 'updated_at',  null: false
-    t.integer  'poll_id'
+  create_table "options", force: :cascade do |t|
+    t.string   "title"
+    t.text     "description"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.integer  "poll_id"
   end
 
-  create_table 'polls', force: :cascade do |t|
-    t.string   'title'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "poles", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
+
+  create_table "polls", force: :cascade do |t|
+    t.string   "title"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
 end
