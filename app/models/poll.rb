b412039ccc1d787cc3c660db75a5db613a428e21
sd-kin <sd-kin@rambler.ru>
2016-05-12 20:36:30 +0000

    add method to make poll ready

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -2,4 +2,16 @@ class Poll < ActiveRecord::Base
   has_many :options, dependent: :destroy
 
   validates :title, presence: true
+
+  def ready!
+    if options.empty? then errors.add(:status, "Can't be ready without options")
+    else 
+      self.status = 'ready'
+      save
+    end
+  end
+
+  def ready? 
+    status == 'ready'
+  end
 end
