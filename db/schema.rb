e732c7f5449b6cbc44f1160fe265c695ff634d14
sd-kin <sd-kin@rambler.ru>
2016-05-12 17:45:22 +0000

    status field for poll. Status needed for check if poll ready for vote, in progress on closed

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151227213610) do
+ActiveRecord::Schema.define(version: 20160512173735) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,16 +24,11 @@ ActiveRecord::Schema.define(version: 20151227213610) do
     t.integer  "poll_id"
   end
 
-  create_table "poles", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "polls", force: :cascade do |t|
     t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                   null: false
+    t.datetime "updated_at",                   null: false
+    t.string   "status",     default: "draft", null: false
   end
 
 end
