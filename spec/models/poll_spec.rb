428c457739f0de62463cc8e5b85ce87f860b95b6
sd-kin <sd-kin@rambler.ru>
2016-05-16 13:25:18 +0000

    tests for poll statuses

--- spec/models/poll_spec.rb
+++ spec/models/poll_spec.rb
@@ -16,4 +16,25 @@ RSpec.describe Poll, type: :model do
   it 'should increase counter' do
     expect { FactoryGirl.create(:valid_poll) }.to change { Poll.count }.by(1)
   end
+
+  it 'should change valid poll status to ready' do
+    poll = FactoryGirl.create(:valid_poll)
+    poll.make_ready
+    expect(poll).to be_ready
+  end
+
+  it 'should not change non valid poll status to ready' do
+    poll = FactoryGirl.create(:poll, :with_title)
+    poll.make_ready
+    expect(poll).not_to be_ready
+  end
+
+  it 'should change poll status from ready to draft when all options deleted' do
+    poll = FactoryGirl.create(:valid_poll)
+    poll.make_ready
+    poll.options.destroy_all
+    poll.touch
+    expect(poll).not_to be_ready
+  end
+
 end
