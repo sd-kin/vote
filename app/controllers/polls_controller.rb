9f852c7096be14e3e7161a3fd7ab02341846aad1
sd-kin <sd-kin@rambler.ru>
2016-05-20 18:08:40 +0000

    remove method with logic before each action, make simple assignment in each method instead

--- app/controllers/polls_controller.rb
+++ app/controllers/polls_controller.rb
@@ -2,53 +2,52 @@
 # frozen_string_literal: true
 class PollsController < ApplicationController
 
-  before_filter :set_poll
-
   def index
     @polls = Poll.all
   end
 
   def show
+    @poll = Poll.find(params[:id])
   end
 
   def new
+    @poll = Poll.new
     @poll.options.build
   end
 
   def edit
+     @poll = Poll.find(params[:id])
   end
 
   def create
+    @poll = Poll.new( poll_params)
     @correct = @poll.save
   end
 
   def update
+    @poll = Poll.find(params[:id])
     @poll.update poll_params
   end
 
   def destroy
+    @poll = Poll.find(params[:id])
     @poll.destroy
     redirect_to polls_path
   end
 
   def make_ready
+    @poll = Poll.find(params[:id])
     @poll.make_ready
     render 'change_status'
   end
 
   def make_draft
+    @poll = Poll.find(params[:id])
     @poll.make_draft
     render 'change_status'
   end
 
   private
-
-  def set_poll
-  @poll = if params[:id] then Poll.find(params[:id])
-             elsif params[:poll] then Poll.new(poll_params)
-             else Poll.new
-             end
-  end
 
   def poll_params
     params.require(:poll).permit(:title)
