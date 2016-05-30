74021d1c6f044dd126bc24c6ad31061cad278e8b
sd-kin <sd-kin@rambler.ru>
2016-05-30 15:34:56 +0000

    try to configure circleCI to load sql structure of db instead of ruby schema because of schema is corrupted because of postgress enum type

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# frozen_string_literal: true
+# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -24,14 +24,7 @@ ActiveRecord::Schema.define(version: 20160530135739) do
     t.integer  "poll_id"
   end
 
-  create_table "polls", force: :cascade do |t|
-    t.string   "title"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.status   "status", default: 'draft'
-  end
-
-  # Could not dump table "polls" because of following StandardError
-  #   Unknown type 'status' for column 'status'
+# Could not dump table "polls" because of following StandardError
+#   Unknown type 'status' for column 'status'
 
 end
