a2628debd14b902d6c119ecd704a4f648b21487f
sd-kin <sd-kin@rambler.ru>
2016-05-30 16:05:40 +0000

    fix rubocop offences

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,4 @@
-# encoding: UTF-8
+# frozen_string_literal: true
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -24,7 +24,7 @@ ActiveRecord::Schema.define(version: 20160530135739) do
     t.integer  "poll_id"
   end
 
-# Could not dump table "polls" because of following StandardError
-#   Unknown type 'status' for column 'status'
+  # Could not dump table "polls" because of following StandardError
+  #   Unknown type 'status' for column 'status'
 
 end
