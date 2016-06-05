70ca835b9dae13f6bc04966f9f8daf888081027b
sd-kin <sd-kin@rambler.ru>
2016-06-05 13:01:01 +0300

    shuffle options list when page load

--- app/assets/javascripts/make_sortable.js
+++ app/assets/javascripts/make_sortable.js
@@ -3,6 +3,14 @@ var ready = function() {
         $(this).css('cursor','move');
                                   });
 
+  $(function () {
+    var parent = $('.sortable');
+    var divs = parent.children();
+    while (divs.length) {
+        parent.append(divs.splice(Math.floor(Math.random() * divs.length), 1)[0]);
+    }
+  });
+
   $('.sortable').sortable();
                        };
 
