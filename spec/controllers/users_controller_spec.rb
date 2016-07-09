c829afc68be765e307961fd86ef0713186408b9b
sd-kin <sd-kin@rambler.ru>
2016-07-09 16:46:41 +0000

    add users_controller tests

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -2,4 +2,19 @@ require 'rails_helper'
 
 RSpec.describe UsersController, type: :controller do
 
+  context 'GET#index' do
+    let!(:user1){FactoryGirl.create(:user)}
+    let!(:user2){FactoryGirl.create(:user)}
+    let!(:user3){FactoryGirl.create(:user)}
+
+    it 'should be success' do 
+      expect(get :index).to be_succes
+    end
+
+    it 'should get all users' do
+      get :index
+      expect(assigns(:users)).to match_array([user1, user2, user3])
+    end
+  end
+
 end
