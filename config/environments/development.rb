ab769ed29b44261d759d59f64f0be0dac32983c9
sd-kin <sd-kin@rambler.ru>
2016-08-01 20:21:35 +0000

    generate mailers

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -39,4 +39,11 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
+
+  # Configure mailers
+
+  config.action_mailer.raise_delivery_errors = true
+  config.action_mailer.delivery_method = :test
+  host = 'localhost:3000'
+  config.action_mailer.default_url_options = { host: host, protocol: 'https' }
 end
---
+++ spec/mailers/user_mailer_spec.rb
@@ -0,0 +1,34 @@
+# frozen_string_literal: true
+require "rails_helper"
+
+RSpec.describe UserMailer, type: :mailer do
+  describe "account_activation" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:mail) { UserMailer.account_activation(user) }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Account activation")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreplay@vote.test"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi")
+    end
+  end
+
+  describe "password_reset" do
+    let(:user) { FactoryGirl.create(:user) }
+    let(:mail) { UserMailer.password_reset user }
+
+    it "renders the headers" do
+      expect(mail.subject).to eq("Password reset")
+      expect(mail.to).to eq([user.email])
+      expect(mail.from).to eq(["noreplay@vote.test"])
+    end
+
+    it "renders the body" do
+      expect(mail.body.encoded).to match("Hi")
+    end
+  end
+end
