82d56426165a4d938bf90d49a91b99ce63ea346c
sd-kin <sd-kin@rambler.ru>
2016-09-20 13:22:01 +0000

    add methods to increase and decrease rating

--- app/models/rating.rb
+++ app/models/rating.rb
@@ -2,4 +2,12 @@ class Rating < ActiveRecord::Base
   belongs_to :rateable, polymorphic: true, autosave: true
   has_many :rates
   has_many :raters, through: :rates, source: :user, foreign_key: 'rater_id'
+
+  def increase
+    self.value += 1
+  end
+
+  def decrease
+    self.value -= 1
+  end
 end
--- spec/models/rating_spec.rb
+++ spec/models/rating_spec.rb
@@ -1,5 +1,19 @@
 require 'rails_helper'
 
 RSpec.describe Rating, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:rating){ Rating.create }
+
+  context 'when change value' do
+    context '#increase' do
+      it 'increase rating by 1' do
+        expect{ rating.increase }.to change{ rating.value }.by(1)
+      end
+    end
+
+    context '#decrease' do
+      it 'decrease rating by 1' do
+        expect{ rating.decrease }.to change{ rating.value }.by(-1)
+      end
+    end
+  end
 end
