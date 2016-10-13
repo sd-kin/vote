06e81c6d199c5b07a59796fc8fed7d11db4f5a4f
sd-kin <sd-kin@rambler.ru>
2016-10-13 12:14:23 +0000

    use validation on delete instead of method overloading

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -7,20 +7,11 @@ class Comment < ActiveRecord::Base
   has_one    :rating, as: :rateable, dependent: :destroy
   has_many   :downvoters, through: :rating, source: :downvoters # users, who decrease comment rating
   has_many   :upvoters, through: :rating, source: :upvoters # users, who increase comment rating
-  has_many   :comments, as: :commentable
+  has_many   :comments, as: :commentable, dependent: :restrict_with_error
 
-  after_create :create_rating # metod aded by ActiveRecord has_one
+  after_create :create_rating # method added by ActiveRecord has_one
 
   def accessible_for?(user)
     user_id == user.id && !user.anonimous?
-  end
-
-  def destroy
-    if comments.empty?
-      super
-    else
-      errors.add(:base, 'Comment with replys can not be deleted')
-      false
-    end
   end
 end
