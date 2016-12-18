af9baac3c6c9b93f976b41e0ad654b4aff1fa6c3
sd-kin <sd-kin@rambler.ru>
2016-12-18 14:44:48 +0000

    no caching in test environment

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -13,9 +13,8 @@ Rails.application.configure do
   # preloads Rails for running tests, you may have to set it to true.
   config.eager_load = false
 
-  # Configure static file server for tests with Cache-Control for performance.
-  config.public_file_server.enabled = true
-  config.public_file_server.headers = { 'Cache-Control' => 'public, max-age=3600' }
+  # Configure static file server for tests.
+  config.public_file_server.enabled = false
 
   # Show full error reports and disable caching.
   config.consider_all_requests_local       = true
