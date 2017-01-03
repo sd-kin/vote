cc6460cd39458c4febc7f54bc715b1e32b7b4e0a
sd-kin <sd-kin@rambler.ru>
2017-01-03 19:58:43 +0300

    configure ActionMailer to use MailGun in production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,4 +77,14 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+  # Use Mailgun add-on on heroku for mailing.
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    port:           ENV['MAILGUN_SMTP_PORT'],
+    addres:         ENV['MAILGUN_SMTP_SERVER'],
+    user_name:      ENV['MAILGUN_SMTP_LOGIN'],
+    password:       ENV['MAILGUN_SMTP_PASSWORD'],
+    domain:         'vote-kompot.herokuapp.com',
+    authentication: :plain
+  }
 end
