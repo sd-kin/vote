bf13d1884f838eee8e5ab0cfa1014af022a258d6
sd-kin <sd-kin@rambler.ru>
2017-03-09 22:47:14 +0000

    add feature spec for comment image

--- spec/features/adding_images_spec.rb
+++ spec/features/adding_images_spec.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 require 'rails_helper'
+include ActionView::RecordIdentifier
 
 RSpec.feature 'Adding images', type: :feature do
   given(:user) { FactoryGirl.create :user }
@@ -7,8 +8,8 @@ RSpec.feature 'Adding images', type: :feature do
 
   before(:each) { login_as(user) }
 
-  feature 'user avatar' do
-    scenario 'user can add and change avatar' do
+  feature 'to user' do
+    scenario '- avatar can be added and changed' do
       visit user_path(user)
 
       expect(page).to have_xpath("//img[contains(@src, '/images/no-profile.jpg')]")
@@ -29,8 +30,8 @@ RSpec.feature 'Adding images', type: :feature do
     end
   end
 
-  feature 'poll image', js: true do
-    scenario 'user can create poll with image and delete it' do
+  feature 'to poll', js: true do
+    scenario '- can create poll with image and delete it' do
       visit new_poll_path
 
       expect(page).to_not have_selector('img')
@@ -43,15 +44,40 @@ RSpec.feature 'Adding images', type: :feature do
       expect(page).to have_xpath("//img[contains(@src, 'medium/test_image.png')]")
 
       click_link 'edit'
-      find_link('delete').click
+      attach_file  'poll_images', 'spec/fixtures/files/test_image2.png'
+      click_button 'Ok'
 
-      expect(page).to_not have_xpath("//img[contains(@src, 'medium/test_image.png')]")
-    end
+      expect(page).to have_xpath("//img[contains(@src, 'medium/test_image.png')]")
+      expect(page).to have_xpath("//img[contains(@src, 'medium/test_image2.png')]")
 
-    scenario 'user can add image to existing poll' do
+      click_link 'edit'
+      click_link('delete image', match: :first)
+      click_button 'Ok'
+
+      expect(page).to_not have_xpath("//img[contains(@src, 'medium/test_image2.png')]")
+      expect(page).to     have_xpath("//img[contains(@src, 'medium/test_image.png')]")
     end
   end
 
-  feature 'comment image' do
+  feature 'to comment', js: true do
+    scenario 'can create comment with image and remove image after' do
+      visit poll_path(poll)
+
+      expect(page).to_not have_selector('img')
+
+      fill_in 'comment[body]', with: 'comment text'
+      attach_file  'comment[images][]', 'spec/fixtures/files/test_image.png'
+      click_button 'Create Comment'
+
+      expect(page).to have_xpath("//img[contains(@src, 'medium/test_image.png')]")
+
+      within '#' + dom_id(poll.comments.last) do
+        click_link 'edit'
+        click_link('delete image')
+        click_button('Update Comment')
+      end
+
+      expect(page).to_not have_xpath("//img[contains(@src, 'medium/test_image.png')]")
+    end
   end
 end
