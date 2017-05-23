8ddd6fa435cb3732525eceee20216b4bc9ba5a5e
Dmitry Sidorov <sd-kin@rambler.ru>
2017-05-23 20:47:09 +0000

    styles for new user form

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -17,3 +17,16 @@
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require_tree .
+
+$(document).on('change', ':file', function() {
+    var input = $(this),
+        numFiles = input.get(0).files ? input.get(0).files.length : 1,
+        label = input.val().replace(/\\/g, '/').replace(/.*\//, '');
+    input.trigger('fileselect', [numFiles, label]);
+});
+
+$(document).ready( function() {
+    $(':file').on('fileselect', function(event, numFiles, label) {
+        $('.file_label').text(label);
+    });
+});
