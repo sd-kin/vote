f100b63959ad9e038aab4c3d6a13b1506f41bbc2
Dmitry Sidorov <sd-kin@rambler.ru>
2017-06-24 19:19:49 +0000

    refactor create user service spec

--- spec/services/users/registration_spec.rb
+++ spec/services/users/registration_spec.rb
@@ -2,6 +2,8 @@
 require 'rails_helper'
 
 RSpec.describe Services::Users::Registration do
+  let(:service_call) { Services::Users::Registration.call(current_user, user_params) }
+
   it 'redirect .call to #call' do
     arguments = [:current_user, :user_params]
     expect_any_instance_of(Services::Users::Registration).to receive(:call).with(*arguments)
@@ -13,42 +15,50 @@ RSpec.describe Services::Users::Registration do
     let(:user_params)   { FactoryGirl.attributes_for(:user) }
 
     it 'return successful result' do
-      expect(Services::Users::Registration.call(current_user, user_params).errors).to be_empty
+      expect(service_call.errors).to be_empty
     end
 
     it 'not increase users counter' do
-      expect { Services::Users::Registration.call(current_user, user_params) }.to_not change { User.count }
+      expect { service_call }.to_not change { User.count }
     end
 
     it 'remove anonymous status' do
-      expect(Services::Users::Registration.call(current_user, user_params)).to_not be_anonimous
+      expect(service_call).to_not be_anonimous
     end
 
     it 'call account activation mailer' do
       expect(UserMailer).to receive(:account_activation).once.and_call_original
 
-      Services::Users::Registration.call(current_user, user_params)
+      service_call
+    end
+
+    it 'generate activation token' do
+      expect(service_call.activation_token).to_not be_empty
+    end
+
+    it 'generate activation digest' do
+      expect(service_call.activation_digest).to_not be_empty
     end
 
     context 'and current user already registered' do
       let!(:current_user) { FactoryGirl.create(:user) }
 
       it 'return successful result' do
-        expect(Services::Users::Registration.call(current_user, user_params).errors).to be_empty
+        expect(service_call.errors).to be_empty
       end
 
       it 'increase users counter' do
-        expect { Services::Users::Registration.call(current_user, user_params) }.to change { User.count }.by(1)
+        expect { service_call }.to change { User.count }.by(1)
       end
 
       it 'return not anonymous user' do
-        expect(Services::Users::Registration.call(current_user, user_params)).to_not be_anonimous
+        expect(service_call).to_not be_anonimous
       end
 
       it 'call account activation mailer' do
         expect(UserMailer).to receive(:account_activation).once.and_call_original
 
-        Services::Users::Registration.call(current_user, user_params)
+        service_call
       end
     end
   end
@@ -58,17 +68,17 @@ RSpec.describe Services::Users::Registration do
     let(:user_params)  { FactoryGirl.attributes_for(:user, email: '') }
 
     it 'return result with errors' do
-      expect(Services::Users::Registration.call(current_user, user_params).errors).to be_any
+      expect(service_call.errors).to be_any
     end
 
     it 'not remove anonymous status' do
-      expect(Services::Users::Registration.call(current_user, user_params)).to be_anonimous
+      expect(service_call).to be_anonimous
     end
 
     it 'do not call account activation mailer' do
       expect(UserMailer).to_not receive(:account_activation)
 
-      Services::Users::Registration.call(current_user, user_params)
+      service_call
     end
   end
 end
