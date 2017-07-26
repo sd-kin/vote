f3dfb505c16da0bc6562a0c18ce6a2b3e0da224d
Dmitry Sidorov <sd-kin@rambler.ru>
2017-07-26 14:26:37 +0000

    show and hide reply form for comment

---
+++ app/assets/javascripts/comments.js
@@ -0,0 +1,31 @@
+
+$(document).on('turbolinks:load', function() {
+	document.querySelectorAll('.reply-link a').forEach(a => a.addEventListener('click', showReplyForm));
+	document.querySelectorAll('.cancel-link a').forEach(a => a.addEventListener('click', hideReplyForm));
+});
+
+function showReplyForm(e) {
+	e.preventDefault();
+
+	const commentID  = e.target.dataset.commentId;
+	const replyForm  = document.querySelector('#reply_form_for_comment_'  + commentID);
+	const replyLink  = this
+	const cancelLink = document.querySelector('#cancel_link_for_comment_' + commentID);
+
+	replyForm.style.display  = 'block'
+	replyLink.style.display  = 'none'
+	cancelLink.style.display = 'block'
+}
+
+function hideReplyForm(e) {
+	e.preventDefault();
+
+	const commentID  = e.target.dataset.commentId;
+	const replyForm  = document.querySelector('#reply_form_for_comment_'  + commentID);
+	const replyLink  = document.querySelector('#reply_link_for_comment_' + commentID);
+	const cancelLink = this
+
+	replyForm.style.display  = 'none'
+	replyLink.style.display  = 'block'
+	cancelLink.style.display = 'none'
+}
