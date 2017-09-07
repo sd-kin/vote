3da6b8e351926dab959306b37de9f45cac61b9e8
desless <sd-kin@rambler.ru>
2017-09-07 10:23:59 +0000

    refactor poll creation service

--- app/services/polls/creation.rb
+++ app/services/polls/creation.rb
@@ -7,30 +7,28 @@ module Services
       include Service
 
       def call(user, params)
-        poll = prepare_poll(user, params)
-        prepare_options(poll, params)
+        poll = prepare_poll_with_options(user, params)
 
-        # TODO: additional validate used because of && short circuit and do not evaluate validation
-        # if left part evaluates to false, refactor that to look more clear. Get rid of side effects in
-        # functions - options_valid? should only return boolean and not set errors or do anything else
-        poll.validate
+        good_to_save = validate_poll_and_options(poll)
 
-        save_poll_with_options(poll) if options_valid?(poll) && poll.valid?
+        save_poll_and_options(poll) if good_to_save
 
-        poll_with_options(poll)
+        poll_with_right_amount_of_options(poll)
       end
 
       private
 
-      def poll_with_options(poll)
+      def poll_with_right_amount_of_options(poll)
         lack_of_options = Poll::MINIMUM_OPTIONS_COUNT - poll.options.size
         lack_of_options.times { poll.options.build }
         poll
       end
 
-      def prepare_poll(user, params)
+      def prepare_poll_with_options(user, params)
         poll_params = params.require(:poll).permit(:title, :max_voters, :expire_at, images: [])
         poll = user.polls.new(poll_params)
+        prepare_options(poll, params)
+        poll
       end
 
       def prepare_options(poll, params)
@@ -45,23 +43,19 @@ module Services
         end
       end
 
-      def options_valid?(poll)
-        validate_options_count(poll) && validate_options(poll)
+      def validate_poll_and_options(poll)
+        poll.validate
+        validate_options_count(poll)
+
+        poll.errors.empty? && poll.options.map(&:valid?).all?
       end
 
       def validate_options_count(poll)
-        if poll.options.size < Poll::MINIMUM_OPTIONS_COUNT
-          poll.errors[:poll] << "should have at least #{Poll::MINIMUM_OPTIONS_COUNT} options"
-          return false
-        end
-        true
+        return if poll.options.size >= Poll::MINIMUM_OPTIONS_COUNT
+        poll.errors[:poll] << "should have at least #{Poll::MINIMUM_OPTIONS_COUNT} options"
       end
 
-      def validate_options(poll)
-        poll.options.map(&:valid?).all?
-      end
-
-      def save_poll_with_options(poll)
+      def save_poll_and_options(poll)
         poll.save
         poll.options.map(&:save)
       end
